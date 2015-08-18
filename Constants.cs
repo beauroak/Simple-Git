d503c41a8668c48b08c79170229107edd7c35e94
beau <bbaudier@dei-engr.com>
2015-08-18 18:39:38 -0500

    added constants

---
+++ Constants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AmbrosiaTrader
+{
+    static class Constants
+    {
+        public static DateTime CloseTradeWarningTime;
+        public static TimeSpan MktOpenTimeSpan;
+        public static DateTime MktOpenTime;
+        public static DateTime MktCloseTime;
+        public static DateTime PreMktOpenTime;
+        public static DateTime PostMktCloseTime;
+
+        // new code
+
+        
+    }
+}
